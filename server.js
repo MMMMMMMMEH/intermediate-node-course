cc8b01d042d5b3c7c6a0a55da50c5666eac5de5b
MMMMMMMMEH <mohammad.oweini@outlook.com>
2020-06-18 10:20:59 +0300

    add update

--- server.js
+++ server.js
@@ -51,5 +51,37 @@ app.route('/users/:id')
     }
   })
 })
+// UPDATE
+.put((req,res)=>{
+  User.findByIdAndUpdate(
+    req.params.id,
+    {
+      name:req.body.newData.name,
+      email:req.body.newData.email,
+      password:req.body.newData.password
+    },
+    {
+      new:true
+    },
+    (err,data)=>{
+      if (err){
+        res.json({
+          success: false,
+          message: err
+        })
+      } else if (!data){
+        res.json({
+          success: false,
+          message: "Not Found"
+        })
+      } else {
+        res.json({
+          success: true,
+          data: data
+        })
+      }
+    }
+  )
+})
 const User=require('./models/User');
 mongoose.connect('mongodb://localhost/userData')
